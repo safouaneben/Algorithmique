3567012d357942db89dec12dc2f793d454eb7119
Safouane Bendou <Safouane.Bendou@grenoble-inp.org>
2021-04-15 21:20:14 +0200

    r

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ sort and display.
 
 from timeit import timeit
 from sys import argv
-from math import sqrt
+
 from geo.point import Point
+from math import sqrt
 
 
 def load_instance(filename):
@@ -19,15 +20,18 @@ def load_instance(filename):
         lines = iter(instance_file)
         distance = float(next(lines))
         points = [Point([float(f) for f in l.split(",")]) for l in lines]
+
     return distance, points
 
-def related(set_a, set_b, points, distance): 
-    min_in_list = sorted([set_a, set_b], key = lambda our_set : len(our_set))
-    for point_a in min_in_list[0]:
-        for point_b in min_in_list[1]:
-            if points[point_a].distance_to(points[point_b]) <= distance:
+def is_in_relation(set1, set2, points, distance):
+    l = sorted([set1, set2], key = lambda sett : len(sett))
+    for point0 in l[0]:
+        for point1 in l[1]:
+            if points[point0].distance_to(points[point1]) <= distance:
                 return True
     return False
+
+
 
 def print_components_sizes(distance, points):
     """
@@ -36,40 +40,45 @@ def print_components_sizes(distance, points):
     if distance == 0:
         print([1 for _ in range(len(points))])
         return
-    plan_division = [[set() for _ in range(int(sqrt(2)/distance) + 1)] for _ in range(int(sqrt(2)/distance) + 1)] 
-    for point_index in range(len(points)):
-        line_index = int((sqrt(2)*points[point_index].coordinates[0])/distance)
-        colomn_index = int((sqrt(2)*points[point_index].coordinates[1])/distance)
-        plan_division[line_index][colomn_index].add(point_index)
-    result = []
-    visited_set = set()
-    nonused_sets = set((i,j) for i in range(len(plan_division)) for j in range(len(plan_division)))
-    counting = 0
-    while counting < len(plan_division)**2:
-        first_set = nonused_sets.pop()
-        if len(plan_division[first_set[0]][first_set[1]]) == 0:
-            counting += 1
-            visited_set.add(first_set)
+    pavage_plan = [[set() for _ in range(int(sqrt(2)/distance) + 1)] for _ in range(int(sqrt(2)/distance) + 1)] 
+    for indice_point in range(len(points)):
+        n = int((sqrt(2)*points[indice_point].coordinates[0])/distance)
+        m = int((sqrt(2)*points[indice_point].coordinates[1])/distance)
+        pavage_plan[n][m].add(indice_point)
+    nombre_traitees = 0
+    element_deja_traitee = set()
+    element_non_traite = set((i,j) for i in range(len(pavage_plan))for j in range(len(pavage_plan)))
+    composantes_connexes = []
+    while nombre_traitees < len(pavage_plan)**2:
+        element_initial = element_non_traite.pop()
+        if len(pavage_plan[element_initial[0]][element_initial[1]]) == 0:
+            nombre_traitees += 1
+            element_deja_traitee.add(element_initial)
             continue
-        pile = [first_set]
-        related_component = []
+        pile = [element_initial]
+        composante_connexe = []
         while len(pile) != 0:
-            just_visited = pile.pop()
-            if len(related_component) == 0:
-                related_component.append(plan_division[just_visited[0]][just_visited[1]])
+            couramment_traitee = pile.pop()
+            if len(composante_connexe) == 0:
+                composante_connexe.append(pavage_plan[couramment_traitee[0]][couramment_traitee[1]])
             else:
-                related_component[0] = related_component[0] | plan_division[just_visited[0]][just_visited[1]]
-            for i in range(-2,3):
-                for j in range(-2,3):
-                    if (i, j)!= (0, 0) and (i, j) not in [(-2,-2),(-2,2),(2,-2),(2,2)] and (just_visited[0] + i, just_visited[1] + j) not in visited_set and 0 <= just_visited[0] + i < len(plan_division) and 0 <= just_visited[1] + j < len(plan_division):
-                        if related(plan_division[just_visited[0]][just_visited[1]], plan_division[just_visited[0] + i][just_visited[1] + j], points, distance):
-                            pile.append((just_visited[0] + i, just_visited[1] + j))
-                            visited_set.add((just_visited[0] + i, just_visited[1] + j))
-                            nonused_sets.remove((just_visited[0] + i, just_visited[1] + j))
-            counting += 1
-            visited_set.add(just_visited)
-        result.append(len(related_component[0]))
-    print(sorted(result)[::-1])
+                composante_connexe[0] = composante_connexe[0] | pavage_plan[couramment_traitee[0]][couramment_traitee[1]]
+            for ligne in range(-2,3):
+                for colonne in range(-2,3):
+                    if (ligne, colonne) != (0,0) and (couramment_traitee[0]+ligne, couramment_traitee[1]+colonne) not in element_deja_traitee and (ligne,colonne) not in [(-2,-2),(-2,2),(2,-2),(2,2)] and 0 <= couramment_traitee[0]+ligne < len(pavage_plan) and 0 <= couramment_traitee[1]+colonne < len(pavage_plan):
+                        if is_in_relation(pavage_plan[couramment_traitee[0]][couramment_traitee[1]], pavage_plan[couramment_traitee[0]+ligne][couramment_traitee[1]+colonne], points, distance):
+                            pile.append((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
+                            element_deja_traitee.add((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
+                            element_non_traite.remove((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
+            nombre_traitees += 1
+            element_deja_traitee.add(couramment_traitee)
+        composantes_connexes.append(len(composante_connexe[0]))
+    print(sorted(composantes_connexes)[::-1])
+
+
+
+
+
 
 def main():
     """
