3a5d8460b791d1566fd36de20aa78ceddcc291dd
Safouane Bendou <Safouane.Bendou@grenoble-inp.org>
2021-04-16 17:47:31 +0200

    retour

--- main.py
+++ main.py
@@ -23,20 +23,14 @@ def load_instance(filename):
 
     return distance, points
 
-def is_in_relation(set1, set2, points, distance):
-    if 0 in [len(set1), len(set2)]:
-        return (False, False)
-    for point0 in set1:
-        for point1 in set2:
-            if points[point0].distance_to(points[point1]) <= distance:
-                return (True, True)
-    return (False, True)
+def related(set_a, set_b, points, distance): 
+    min_in_list = sorted([set_a, set_b], key = lambda our_set : len(our_set))
+    for point_a in min_in_list[0]:
+        for point_b in min_in_list[1]:
+            if points[point_a].distance_to(points[point_b]) <= distance:
+                return True
+    return False
     
-def signe(entier):
-    if entier == 0:
-        return 0
-    else:
-        return int(abs(entier)/entier)
 
 
 
@@ -47,55 +41,40 @@ def print_components_sizes(distance, points):
     if distance == 0:
         print([1 for _ in range(len(points))])
         return
-    pavage_plan = [[set() for _ in range(int(sqrt(2)/distance) + 1)] for _ in range(int(sqrt(2)/distance) + 1)] 
-    for indice_point in range(len(points)):
-        n = int((sqrt(2)*points[indice_point].coordinates[0])/distance)
-        m = int((sqrt(2)*points[indice_point].coordinates[1])/distance)
-        pavage_plan[n][m].add(indice_point)
-    nombre_traitees = 0
-    element_deja_traitee = set()
-    element_non_traite = set((i,j) for i in range(len(pavage_plan))for j in range(len(pavage_plan)))
-    composantes_connexes = []
-    while nombre_traitees < len(pavage_plan)**2:
-        element_initial = element_non_traite.pop()
-        if len(pavage_plan[element_initial[0]][element_initial[1]]) == 0:
-            nombre_traitees += 1
-            element_deja_traitee.add(element_initial)
+    plan_division = [[set() for _ in range(int(sqrt(2)/distance) + 1)] for _ in range(int(sqrt(2)/distance) + 1)] 
+    for point_index in range(len(points)):
+        line_index = int((sqrt(2)*points[point_index].coordinates[0])/distance)
+        colomn_index = int((sqrt(2)*points[point_index].coordinates[1])/distance)
+        plan_division[line_index][colomn_index].add(point_index)
+    result = []
+    visited_set = set()
+    nonused_sets = set((i,j) for i in range(len(plan_division)) for j in range(len(plan_division)))
+    counting = 0
+    while counting < len(plan_division)**2:
+        first_set = nonused_sets.pop()
+        if len(plan_division[first_set[0]][first_set[1]]) == 0:
+            counting += 1
+            visited_set.add(first_set)
             continue
-        pile = [element_initial]
-        composante_connexe = 0
+        pile = [first_set]
+        related_component = []
         while len(pile) != 0:
-            couramment_traitee = pile.pop()
-            composante_connexe += len(pavage_plan[couramment_traitee[0]][couramment_traitee[1]])
-            a_ne_pas_traiter = [(-2,-2),(-2,2),(2,-2),(2,2)]
-            for ligne in [0, -1,-2, 1, 2]:
-                for colonne in [0, -1, -2 , 1, 2]:
-                    if (ligne, colonne) != (0,0) and (couramment_traitee[0]+ligne, couramment_traitee[1]+colonne) not in element_deja_traitee and (ligne,colonne) not in a_ne_pas_traiter and 0 <= couramment_traitee[0]+ligne < len(pavage_plan) and 0 <= couramment_traitee[1]+colonne < len(pavage_plan):
-                        in_relation = is_in_relation(pavage_plan[couramment_traitee[0]][couramment_traitee[1]], pavage_plan[couramment_traitee[0]+ligne][couramment_traitee[1]+colonne], points, distance)
-                        if in_relation[0]:
-                            pile.append((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
-                            element_deja_traitee.add((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
-                            element_non_traite.remove((couramment_traitee[0]+ligne, couramment_traitee[1]+colonne))
-                        if not in_relation[0] and in_relation[1]:
-                            if ligne == 0:
-                                for h in range(-1,2):
-                                    a_ne_pas_traiter.append((h, colonne + signe(colonne)))
-                            if colonne == 0:
-                                for r in range(-1,2):
-                                    a_ne_pas_traiter.append((ligne + signe(ligne), r))
-                                    
-                                    
-                                    
-            nombre_traitees += 1
-            element_deja_traitee.add(couramment_traitee)
-        composantes_connexes.append(composante_connexe)
-    print(sorted(composantes_connexes)[::-1])
-
-
-
-
-
-
+            just_visited = pile.pop()
+            if len(related_component) == 0:
+                related_component.append(plan_division[just_visited[0]][just_visited[1]])
+            else:
+                related_component[0] = related_component[0] | plan_division[just_visited[0]][just_visited[1]]
+            for i in range(-2,3):
+                for j in range(-2,3):
+                    if (i, j)!= (0, 0) and (i, j) not in [(-2,-2),(-2,2),(2,-2),(2,2)] and (just_visited[0] + i, just_visited[1] + j) not in visited_set and 0 <= just_visited[0] + i < len(plan_division) and 0 <= just_visited[1] + j < len(plan_division):
+                        if related(plan_division[just_visited[0]][just_visited[1]], plan_division[just_visited[0] + i][just_visited[1] + j], points, distance):
+                            pile.append((just_visited[0] + i, just_visited[1] + j))
+                            visited_set.add((just_visited[0] + i, just_visited[1] + j))
+                            nonused_sets.remove((just_visited[0] + i, just_visited[1] + j))
+            counting += 1
+            visited_set.add(just_visited)
+        result.append(len(related_component[0]))
+    print(sorted(result)[::-1])
 def main():
     """
     ne pas modifier: on charge une instance et on affiche les tailles
